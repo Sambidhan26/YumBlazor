7aaea90317246ff710341c15d428a38ab76097f9
Sambidhan26 <sambidhan26@gmail.com>
2025-08-24 08:55:45 +0545

    Notification Implementation Section-12

--- YumBlazor/Data/ApplicationDbContext.cs
+++ YumBlazor/Data/ApplicationDbContext.cs
@@ -5,5 +5,18 @@ namespace YumBlazor.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Category> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Appetizers" },
+                new Category { Id = 2, Name = "Main Courses" },
+                new Category { Id = 3, Name = "Desserts" },
+                new Category { Id = 4, Name = "Beverages" }
+            );
+        }
     }
 }
